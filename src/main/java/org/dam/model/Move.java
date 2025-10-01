f483b86af3ffaf12c255f68114cb3f539a28b6ce
Mahu2121 <hugogbmahu@gmail.com>
2025-10-01 23:38:26 +0200

    feat(Move): añadir clase move con sus variables

--- src/main/java/org/dam/model/Move.java
+++ src/main/java/org/dam/model/Move.java
@@ -1,23 +1,57 @@
 package org.dam.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public class Move {
 
-    private String type;
-    private String name;
-    private String machine;
-    private Integer level;
-    private List<Move> moves;
+    @JsonProperty("type")
+    private final String type;
 
-    public Move(String type, String name, String machine, Integer level, List<Move> moves) {
+    @JsonProperty("name")
+    private final String name;
+
+    @JsonProperty("machine")
+    private final String machine;
+
+    @JsonProperty("level")
+    private final Integer level;
+
+
+    @JsonCreator
+    public Move(
+            @JsonProperty("type") String type,
+            @JsonProperty("name") String name,
+            @JsonProperty("machine") String machine,
+            @JsonProperty("level") Integer level
+    ) {
         this.type = type;
         this.name = name;
         this.machine = machine;
         this.level = level;
-        this.moves = moves;
     }
 
 
+    public Move(String type, String name) {
+        this.type = type;
+        this.name = name;
+        this.machine = null;
+        this.level = null;
+    }
 
+
+    public String getType() {
+        return type;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getMachine() {
+        return machine;
+    }
+    public Integer getLevel() {
+        return level;
+    }
 }
